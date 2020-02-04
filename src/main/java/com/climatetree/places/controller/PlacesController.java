a5226aaa67ce8835a29e4b9b26538270fa5a29d0
colemclark1 <clark.cole@husky.neu.edu>
2020-02-03 19:14:58 -0500

    getSimilarPlaces API

--- src/main/java/com/climatetree/places/controller/PlacesController.java
+++ src/main/java/com/climatetree/places/controller/PlacesController.java
@@ -1,12 +1,16 @@
 package com.climatetree.places.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.climatetree.places.dto.PlaceDTO;
 import com.climatetree.places.model.PlaceInfo;
 import com.climatetree.places.service.PlacesService;
 
@@ -21,6 +25,11 @@ public class PlacesController {
 	@GetMapping("/places/{placeId}")
 	public PlaceInfo getAllPlaces(@PathVariable("placeId") int placeId) {
 		return placesService.findPlaceById(placeId);
+	}
+	
+	@GetMapping("/places/{placeId}/similar")
+	public List<PlaceInfo> getSimilarPlaces(@PathVariable("placeId") int placeId, @RequestBody PlaceDTO place) {
+		return placesService.getSimilarPlaces(place);
 	}
 
 }
--- src/main/java/com/climatetree/places/service/PlacesService.java
+++ src/main/java/com/climatetree/places/service/PlacesService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.climatetree.places.dao.PlacesRepository;
+import com.climatetree.places.dto.PlaceDTO;
 import com.climatetree.places.model.PlaceInfo;
 
 @Service
@@ -24,6 +25,18 @@ public class PlacesService {
 	public List<PlaceInfo> findAllPlaces() {
 		List<PlaceInfo> places = new ArrayList<>();
 		placesRepo.findAll().forEach(p -> places.add(p));
+		return places;
+	}
+	
+	public List<PlaceInfo> getSimilarPlaces(PlaceDTO place) {
+		List<PlaceInfo> places = new ArrayList<>();
+		double pop_start = place.getPopulation() * .95;
+		double pop_end = place.getPopulation() * 1.25;
+		double popDensity_start = place.getPopulation() * .95;
+		double popDensity_end = place.getPopulation() * 1.25;
+		
+		this.placesRepo.getSimilarPlaces(pop_start, pop_end, popDensity_start, popDensity_end).forEach(p -> places.add(p));
+		
 		return places;
 	}
 
