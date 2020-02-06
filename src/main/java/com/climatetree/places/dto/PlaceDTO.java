cba5b7df8a4b5a72efc872c9952c8ffe421e59d5
colemclark1 <clark.cole@husky.neu.edu>
2020-02-05 20:34:34 -0500

    Fixed and added tests

--- src/main/java/com/climatetree/places/dto/PlaceDTO.java
+++ src/main/java/com/climatetree/places/dto/PlaceDTO.java
@@ -83,4 +83,57 @@ public class PlaceDTO {
 		this.popdensity = popdensity;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(carbon);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		temp = Double.doubleToLongBits(percapcarb);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + placeId;
+		temp = Double.doubleToLongBits(popdensity);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(population);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((typeName == null) ? 0 : typeName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PlaceDTO other = (PlaceDTO) obj;
+		if (Double.doubleToLongBits(carbon) != Double.doubleToLongBits(other.carbon))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (Double.doubleToLongBits(percapcarb) != Double.doubleToLongBits(other.percapcarb))
+			return false;
+		if (placeId != other.placeId)
+			return false;
+		if (Double.doubleToLongBits(popdensity) != Double.doubleToLongBits(other.popdensity))
+			return false;
+		if (Double.doubleToLongBits(population) != Double.doubleToLongBits(other.population))
+			return false;
+		if (typeName == null) {
+			if (other.typeName != null)
+				return false;
+		} else if (!typeName.equals(other.typeName))
+			return false;
+		return true;
+	}
+
+	
+	
 }
---
+++ src/test/java/com/climatetree/places/model/PlaceInfoTest.java
@@ -0,0 +1,85 @@
+package com.climatetree.places.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.climatetree.places.dto.PlaceDTO;
+
+class PlaceInfoTest {
+	
+	PlaceInfo input;
+	
+	@BeforeEach
+	void setup() {
+		Type type = new Type(1, "type", new HashSet<PlaceInfo>());
+		EcoName ecoName = new EcoName(2, "ecoName", new HashSet<PlaceInfo>());
+		WwfMhtnam wwfmhtnam = new WwfMhtnam(3, "wwfmhtnam", new HashSet<PlaceInfo>());
+		WwfRealm2 wwfrealm2 = new WwfRealm2(4, "wwfrealm2", new HashSet<PlaceInfo>());
+		input = new PlaceInfo(0, type, ecoName, wwfmhtnam, wwfrealm2, 1.0, 2.0, 3.0, 4.0, "hasc1", 5.0, 6.0);
+	}
+	
+
+	@Test
+	void testConvertToDTO1() {
+		PlaceDTO expected = new PlaceDTO(0, null, "type", 1.0, 2.0, 3.0, 4.0);
+		PlaceDTO actual = input.convertToPlaceDTO();
+		
+		// Assert
+		assertThat(actual).isEqualTo(expected);
+		
+	}
+	
+	@Test
+	void testConvertToDTO2() {
+		HashSet<Name> names = new HashSet<>();
+		Name name = new Name();
+		name.setName("USA");
+		names.add(name);
+		input.setNames(names);
+		PlaceDTO expected = new PlaceDTO(0, "USA", "type", 1.0, 2.0, 3.0, 4.0);
+		PlaceDTO actual = input.convertToPlaceDTO();
+		
+		// Assert
+		assertThat(actual).isEqualTo(expected);
+		
+	}
+	
+	@Test
+	void testConvertToDTO3() {
+		input.setNames(new HashSet<Name>());
+		PlaceDTO expected = new PlaceDTO(0, null, "type", 1.0, 2.0, 3.0, 4.0);
+		PlaceDTO actual = input.convertToPlaceDTO();
+		
+		// Assert
+		assertThat(actual).isEqualTo(expected);
+		
+	}
+	
+	@Test
+	void testConvertToDTO4() {
+		input.setType(new Type());
+		PlaceDTO expected = new PlaceDTO(0, null, null, 1.0, 2.0, 3.0, 4.0);
+		PlaceDTO actual = input.convertToPlaceDTO();
+		
+		// Assert
+		assertThat(actual).isEqualTo(expected);
+		
+	}
+	
+	@Test
+	void testConvertToDTO5() {
+		input.setType(null);
+		PlaceDTO expected = new PlaceDTO(0, null, null, 1.0, 2.0, 3.0, 4.0);
+		PlaceDTO actual = input.convertToPlaceDTO();
+		
+		// Assert
+		assertThat(actual).isEqualTo(expected);
+		
+	}
+
+}
