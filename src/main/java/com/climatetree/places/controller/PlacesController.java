f27064c11968bf52ad0e495c3ea8aaf165627450
priyankadebnath <priyankadebnath@icloud.com>
2020-02-07 12:38:02 -0500

    removed unused endpoint

--- src/main/java/com/climatetree/places/controller/PlacesController.java
+++ src/main/java/com/climatetree/places/controller/PlacesController.java
@@ -2,21 +2,16 @@ package com.climatetree.places.controller;
 
 
 import java.util.List;
-
 import java.util.Set;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import com.climatetree.places.dto.PlaceDTO;
-import com.climatetree.places.model.PlaceInfo;
 import com.climatetree.places.service.NamesService;
 import com.climatetree.places.service.PlacesService;
 
@@ -30,11 +25,6 @@ public class PlacesController {
 	@Autowired
 	NamesService namesService;
 
-	@GetMapping("/places/{placeId}")
-	public PlaceInfo getAllPlaces(@PathVariable("placeId") int placeId) {
-		return placesService.findPlaceById(placeId);
-	}
-	
 	@GetMapping("/places/{placeId}/similar")
 	public List<PlaceDTO> getSimilarPlaces(@PathVariable("placeId") int placeId, @RequestBody PlaceDTO place) {
 		return placesService.getSimilarPlaces(place);
