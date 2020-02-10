026ac7695e263f95e2b708800753cf87c02475fb
patelvp <patel.visha@husky.neu.edu>
2020-02-10 10:50:48 -0500

    Push only the built image

--- docker_push.sh
+++ docker_push.sh
@@ -2,4 +2,4 @@
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t climatetree:places .
 docker tag climatetree:places patelvp/climatetree:places
-docker push patelvp/climatetree
+docker push patelvp/climatetree:places
