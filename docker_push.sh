f7c2b5c5667064c5ee4fcd5fcadda068a9692012
patelvp <patel.visha@husky.neu.edu>
2020-02-16 19:09:08 -0500

    Tag images correctly

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t climatetree:places .
-docker tag climatetree:places patelvp/climatetree:places
-docker push patelvp/climatetree:places
+docker build -t climatetree-places .
+docker tag climatetree-places patelvp/climatetree-places
+docker push patelvp/climatetree-places
